baf3f95168b820e4550ded44cf941c2c0cb5b5d4
이기수 <leekisu@igisuui-MacBookAir.local>
2021-03-18 14:48:36 +0900

    fst settings

---
+++ UpDownGame/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  UpDownGame
+//
+//  Created by 이기수 on 2021/03/16.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
