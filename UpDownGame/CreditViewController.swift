d1251ed309ef6be541d04a8b29682280adce5249
이기수 <leekisu@igisuui-MacBookAir.local>
2021-03-19 11:00:37 +0900

    add credit

---
+++ UpDownGame/CreditViewController.swift
@@ -0,0 +1,17 @@
+//
+//  CreditViewController.swift
+//  UpDownGame
+//
+//  Created by 이기수 on 2021/03/19.
+//
+
+import UIKit
+
+class CreditViewController: UIViewController {
+
+    @IBAction func touchUpDismissButton(_ sender: UIButton) {
+        dismiss(animated: true,
+                completion: nil)
+    }
+
+}
